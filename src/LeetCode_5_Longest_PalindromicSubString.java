5d00fa510d507de327cc7a03015997bf8687629b
jaehyukjang <culdm@naver.com>
2021-07-20 20:45:11 +0900

    LeetCode_5_Longest_Palindromic_SubString

---
+++ src/LeetCode_5_Longest_PalindromicSubString.java
@@ -0,0 +1,36 @@
+public class LeetCode_5_Longest_PalindromicSubString {
+    public static void main(String...msg){
+       String s ="aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabcaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+       // String s = "babad";
+        System.out.println(longestPalindrome(s));
+    }
+    static int start,end;
+    public static String longestPalindrome(String s) {
+        for(int i = 0 ; i < s.length()-1; i++){
+            isPalindrome(s, i, i, i);
+            isPalindrome(s, i,i+1, i);
+        }
+        System.out.println(start+" / "+end);
+        return  s.substring(start, end+1);
+    }
+
+    public static void isPalindrome(String s, int st, int e, int i){
+        while(st >= 0 && e <s.length() && s.charAt(st) == s.charAt(e)) {
+            st--;
+            e++;
+        }
+        st++;
+        e--;
+        if(e - st > end-start) {
+            start = st;
+            end = e;
+        }
+    }
+}
+
+/*
+    0.0 , 0,1 . 0,2 . 0.3 , 0.4. 0,5
+    1,1
+    1,1 , 1,2 , 1,3 , 1,4 , 1,5
+    2,2 , 2,3 , 2,4 , 2,5
+ */
